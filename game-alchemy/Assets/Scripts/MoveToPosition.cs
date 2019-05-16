698037505c3a697e7350bed4ed2e9a846910bbc0
MattheW Ray <mray@zoll.com>
2019-05-15 20:50:04 -0400

    051519 - commit

---
+++ game-alchemy/Assets/Scripts/MoveToPosition.cs
@@ -0,0 +1,29 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class MoveToPosition : MonoBehaviour {
+ 
+    public float speed = 1.0f;
+    private Vector3 target;
+ 
+    private Animator anim;
+
+    void Start () {
+        target = transform.position;
+        anim = GetComponent<Animator>();
+    }
+     
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        
+        if(transform.position == target){
+            anim.SetBool("isWalking", false);
+        }else{
+            anim.SetBool("isWalking", true);
+        }
+    }    
+ }
